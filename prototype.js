6ea5d6ac9723894eca12a1b2fddffe2ef523987b
Ana Glória <147454263+anagloria22@users.noreply.github.com>
2024-07-15 22:59:42 -0300

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -16,3 +16,14 @@ console.log(robson.prototype)
 //funcao.protoype
 //podemos adicionar novas propriedades e métodos ao objeto prototype
 //primeiro a verificação é feita no objeto e depois é que vai pro prototipo caso nao tenha no objeto
+
+const país = 'Brasil'
+const cidade = new String('Sobral')
+
+const listaAnimais = ['Cachorro', 'Gato', 'Cavalo']
+
+const listaArray1 = Array.prototype.slice.call(lista) //está linkado ao prototipo
+const listaArray2 = Array.from(lista) //é uma forma mais rápida e está linkada direto com a função array
+
+Object.getOwnPropertyNames(Array) //vai retornar uma lista com os métodos/propriedades
+//dado.constructor.name retorna o nome do construtor
